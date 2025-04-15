a9fe3daa063a50a5ee0651014981063fa9fcc257
kiokahn <kiokahn@gazzi.ai>
2025-04-15 13:32:13 +0900

    modified source

--- app/src/main/java/com/example/project9_1/MainActivity.java
+++ app/src/main/java/com/example/project9_1/MainActivity.java
@@ -71,16 +71,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.option_menu, menu);
-        return true;
-    }
-    */
-
-
-    /*
-    */
 
